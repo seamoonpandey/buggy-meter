74fad9322decc2a44ac075df219647f86c245fed
Seamoon Pandey <115852972+seamoonpandey@users.noreply.github.com>
2023-11-03 07:40:23 -0400

    [SP] Add user view (#1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   get 'home/index'
   root to: 'home#index'
+
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
