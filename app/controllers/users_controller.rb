7c089470faf93aee8acf54f04c7cd17cf8f97c61
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-03 20:13:03 +0545

    add user profile everywhere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def show
-        @posts = current_user.posts.order(created_at: :desc)
+        @user  = User.find(params[:id])
+        @posts = @user.posts.order(created_at: :desc)
     end
     def edit
         @user = User.find(params[:id])
@@ -14,6 +15,6 @@ class UsersController < ApplicationController
     private
     def user_params
     params.require(:user).permit(:username, :name, :website,
-                                :bio, :email, :phone, :gender)
+                                :bio, :email, :phone, :gender, :avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  has_one_attached :avatar
+
 end
