359bece0dcbb132875dc8f816cce9dfbd2f71270
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-03 18:47:17 +0545

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :users, only: [:show, :edit, :update]
+
+  resources :posts, only: [:new, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
