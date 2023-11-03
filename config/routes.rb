84d94fbb22fa2aeb1cb0239cc43d32e428772356
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-03 20:47:56 +0545

    add show functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
 
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :show, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
