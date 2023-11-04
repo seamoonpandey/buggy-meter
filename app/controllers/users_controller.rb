db66985d228326c4526a7f7cc6658bfb7d6d2e27
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-04 09:18:47 +0545

    set follower route and controller in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+
+    before_action :find_user, only: [:show, :edit, :update, :follow, :unfollow]
+    
     def show
-        @user  = User.find(params[:id])
         @posts = @user.posts.order(created_at: :desc)
     end
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
@@ -12,9 +13,22 @@ class UsersController < ApplicationController
         redirect_to current_user
     end
 
+    def follow
+        current_user.followees << @user
+        redirect_back(fallback_location: user_path(@user))
+    end
+    
+    def unfollow
+        current_user.followed_users.find_by(followee_id: @user.id).destroy
+        redirect_back(fallback_location: user_path(@user))
+    end
+
     private
     def user_params
     params.require(:user).permit(:username, :name, :website,
                                 :bio, :email, :phone, :gender, :avatar)
+    end
+    def find_user
+        @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :follows
-  devise_for :users
   
   get 'home/index'
   root to: 'home#index'
+  
+  devise_for :users
 
   resources :users, only: [:show, :edit, :update]
+  post '/users/:id/follow', to: "users#follow", as: "follow_user"
+  post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
 
   resources :posts, only: [:new, :create, :show, :destroy]
 
