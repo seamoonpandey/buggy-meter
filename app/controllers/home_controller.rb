07f953e7b369cf2b1f3e8bef207e91d8faadbbdc
Seamoon Pandey <hackedasocial@gmail.com>
2023-11-09 12:36:09 +0545

    add post serializer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    if current_user
+      priority_posts = Post.where(user: [current_user,current_user.followees]).order(created_at: :desc)
+      other_posts = Post.where.not(user: [current_user, current_user.followees]).order(created_at: :desc)
+      @posts = priority_posts + other_posts
+    else
+      @posts = Post.order(created_at: :desc)
+    end
   end
 end
